393372812cef10261e9627fb3df0e97955cff874
Serg-13 <grey.panchenko@gmail.com>
2017-02-13 12:42:48 +0200

    configure Mysql

--- dayside/index.php
+++ dayside/index.php
@@ -7,17 +7,17 @@
             <script tea="server/assets/index.tea"></script>
             <script src="server/assets/teacss/teacss.js"></script>
         <? else: ?>
-            <script src="server/assets/teacss/teacss.js"></script>
-            <script src="server/assets/teacss/teacss-ui.js"></script>
-            <link href="server/assets/teacss/teacss-ui.css" rel="stylesheet" type="text/css">
-            <script src="client/dayside.js"></script>
-            <link href="client/dayside.css" rel="stylesheet" type="text/css">
-            <script src="plugins/pixlr/pixlr.js"></script>
-            <script src="plugins/xdebug/xdebug.js"></script>
-            <link href="plugins/xdebug/xdebug.css" rel="stylesheet" type="text/css">
-            <script src="plugins/console/console.js"></script>
-            <script src="plugins/git_commit/git_commit.js"></script>
-            <link href="plugins/git_commit/git_commit.css" rel="stylesheet" type="text/css">
+            <script src="dayside/server/assets/teacss/teacss.js"></script>
+            <script src="dayside/server/assets/teacss/teacss-ui.js"></script>
+            <link href="dayside/server/assets/teacss/teacss-ui.css" rel="stylesheet" type="text/css">
+            <script src="dayside/client/dayside.js"></script>
+            <link href="dayside/client/dayside.css" rel="stylesheet" type="text/css">
+            <script src="dayside/plugins/pixlr/pixlr.js"></script>
+            <script src="dayside/plugins/xdebug/xdebug.js"></script>
+            <link href="dayside/plugins/xdebug/xdebug.css" rel="stylesheet" type="text/css">
+            <script src="dayside/plugins/console/console.js"></script>
+            <script src="dayside/plugins/git_commit/git_commit.js"></script>
+            <link href="dayside/plugins/git_commit/git_commit.css" rel="stylesheet" type="text/css">
             <!--<script src="plugins/collaborate/collaborate.js"></script>-->
 
             <script>
